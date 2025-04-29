0a524e347940cc257a55e17f13645d33d8766563
Evgallos <evangelos.gallos@gmail.com>
2025-04-29 09:41:45 +0200

    Book Controller & Customer Controller

--- OnlineBookShop/Models/Book.cs
+++ OnlineBookShop/Models/Book.cs
@@ -6,5 +6,9 @@
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        public string Category {  get; set; } = string.Empty;
+
     }
 }
