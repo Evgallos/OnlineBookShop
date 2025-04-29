a48413040027a65a9c459c6993bb433d77135278
Evgallos <evangelos.gallos@gmail.com>
2025-04-29 08:42:20 +0200

    Add project files.

---
+++ OnlineBookShop/Data/OnlineBookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookShop.Models;
+
+namespace OnlineBookShop.Data
+{
+    public class OnlineBookShopContext : DbContext
+    {
+        public OnlineBookShopContext(DbContextOptions<OnlineBookShopContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
