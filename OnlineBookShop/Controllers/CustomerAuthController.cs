825b84433abb6ca8d27fc8612d010f20ccd3a110
Evgallos <evangelos.gallos@gmail.com>
2025-04-29 10:39:01 +0200

    Added customer Auth Register & Log in Updated Database and changed the Name to Username from classes and small changes to _context

---
+++ OnlineBookShop/Controllers/CustomerAuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookShop.Data;
+using OnlineBookShop.Models;
+
+namespace OnlineBookShop.Controllers
+{
+    public class CustomerAuthController : Controller
+    {
+        private readonly OnlineBookShopContext _context;
+
+        public CustomerAuthController(OnlineBookShopContext context)
+        {
+            _context = context;
+        }
+
+        // ---- Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View(customer);
+        }
+
+        //---------- Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var customer = _context.Customers
+                .FirstOrDefault(c => c.Username == username && c.Password == password);
+            if (customer != null)
+            {
+                return RedirectToAction("Index", "Books");
+            }
+            ViewBag.Error = "Invalid login. Try again";
+            return View();
+        }
+    }
+}
--- OnlineBookShop/Models/Customer.cs
+++ OnlineBookShop/Models/Customer.cs
@@ -1,10 +1,21 @@
-﻿namespace OnlineBookShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBookShop.Models
 {
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
         public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
 
 
